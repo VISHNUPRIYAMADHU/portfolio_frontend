696e5c4d48758bc612b23f54d1e9d5c95f000433
Vishnupriya <vishnupriyamadhu1996@gmail.com>
2025-04-07 22:20:18 +0200

    changed backend url

--- src/pages/Chatbot.js
+++ src/pages/Chatbot.js
@@ -36,7 +36,7 @@ const Chatbot = () => {
 
   const calculateMatchScore = async (finalResponses) => {
     try {
-      const response = await fetch('http://localhost:5000/calculate-match', {
+      const response = await fetch('https://portfolio-backend-kufc.onrender.com/calculate-match', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ responses: finalResponses }),  // Send finalResponses as array of arrays
